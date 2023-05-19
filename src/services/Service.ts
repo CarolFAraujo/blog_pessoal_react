65493bc6d279f7265714b7180fbbf62851d81750
CarolFAraujo <carolina.fac@gmail.com>
2023-05-19 15:06:54 -0300

    Atualização Blog Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://blogpessoal-k3k7.onrender.com'
+    baseURL: ''
 })
 
     export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
